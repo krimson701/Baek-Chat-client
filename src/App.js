5281620e4ee067b6987a81e9c9419189371c9e02
krimson701 <krimson701@gmail.com>
2020-05-24 17:03:35 +0900

    로그인테스트
    채팅방은 잠시 보류

--- src/App.js
+++ src/App.js
@@ -1,78 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import SockJsClient from "react-stomp";
-import UsernameGenerator from "username-generator";
-import Fetch from "json-fetch";
-import { TalkBox } from "react-talk";
-import randomstring from"randomstring";
+import axios from 'axios';
+import { GoogleLogin } from 'react-google-login';
+
+/**
+ *  재훈
+ *  원래 HomePage이나 아직 지식부족으로 잠시 보관해놓고
+ *  다른기능 구현되면 그때 제대로 갖다 쓸예정입니다. 
+ *  Login, Channel Api 구현후 추가될 예정
+ */
 
 class App extends React.Component {
- 
+
   constructor(props) {
     super(props);
     // randomUserId is used to emulate a unique user id for this demo usage
-    this.randomUserName = UsernameGenerator.generateUsername("-");
-    this.randomUserId = randomstring.generate();
-    this.channelNo = 1;
-    this.sendURL = "/message";
+    this.UserName = "";
+    this.UserEmail = "";
+    this.UserHobby = "";
     this.state = {
-      clientConnected : false,
-      messages : []
+      accessToken : ""
     };
-    
   }
-
-  onMessageReceive = (msg, topic) => {
-    //alert(JSON.stringify(msg) + " @ " +  JSON.stringify(this.state.messages)+" @ " + JSON.stringify(topic));
-    this.setState(prevState => ({
-      messages: [...prevState.messages, msg]
-    }));
+  //함수
+responseGoogle = (response) => {
+  console.log(response);
+  const headers = {
+    'Content-Type': 'application/json;charset=UTF-8',
+    'Authorization': response.accessToken
   }
-
-  sendMessage = (msg, selfMsg) => {
-    try {
-      var send_message = {
-        "user" : selfMsg.author,
-        "message" : selfMsg.message,
-        "channelNo" : this.channelNo
-      }
-      this.clientRef.sendMessage("/app/message", JSON.stringify(send_message));
-      return true;
-    } catch(e) {
-      return false;
-    }
-  }
-  
-  componentWillMount() {
-    console.log("call history");
-    Fetch("http://ec2-18-213-155-163.compute-1.amazonaws.com:9090/history/"+this.channelNo , {
-      method: "GET"
-    }).then((response) => {
-      response.body.forEach(element => {
-        element.author=element.userNo;  // 추후 서버에서 userNo으로 userName을 찾아서 같이 내려올예정 (몽고db에 같이넣을까 체크해바야함)
-      });
-      this.setState({ messages: response.body.reverse() });
-      console.log(response.body)
-      
+  console.log(response.accessToken);
+  axios.post(
+    'http://localhost:9090/login/signIn',"data", {
+      headers: headers
+    }).then(response => {
+      this.setState({accessToken : response.accessToken })
     });
-  }
+};
+
+responsefail = () => {
+alert("로그인 실패");
+};
+
+//태그
 
   render() {
-    const wsSourceUrl = "http://ec2-18-213-155-163.compute-1.amazonaws.com:9090/chatting";
-    return (
+    return(
       <div>
-        <TalkBox topic={"/topic/public/"+this.channelNo} currentUserId={ this.randomUserId }
-          currentUser={ this.randomUserName } messages={ this.state.messages }
-          onSendMessage={ this.sendMessage } connected={ this.state.clientConnected }/>
-        
-        <SockJsClient url={ wsSourceUrl } topics={["/topic/public/"+this.channelNo]}
-          onMessage={ this.onMessageReceive } ref={ (client) => { this.clientRef = client }}
-          onConnect={ () => {this.setState({ clientConnected: true }) } }
-          onDisconnect={ () => { this.setState({ clientConnected: false }) } }
-          debug={ false } style={[{width:'100%', height:'100%'}]}/>
+      <GoogleLogin
+        clientId="93172012175-1us86pmqgilm2kg38knddg8g72d3jari.apps.googleusercontent.com"
+        render={(renderProps) => (
+          <button
+            onClick={renderProps.onClick}
+            disabled={renderProps.disabled}
+          >
+            Google로 LogIn
+          </button>
+        )}
+        onSuccess={this.responseGoogle}
+        onFailure={this.responsefail}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn={true} 
+      />
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/App3.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import './App.css';
+import SockJsClient from "react-stomp";
+import UsernameGenerator from "username-generator";
+import Fetch from "json-fetch";
+import { TalkBox } from "react-talk";
+import randomstring from"randomstring";
+
+class App extends React.Component {
+ 
+  constructor(props) {
+    super(props);
+    // randomUserId is used to emulate a unique user id for this demo usage
+    this.randomUserName = UsernameGenerator.generateUsername("-");
+    this.randomUserId = randomstring.generate();
+    this.channelNo = 1;
+    this.sendURL = "/message";
+    this.state = {
+      clientConnected : false,
+      messages : []
+    };
+    
+  }
+
+  onMessageReceive = (msg, topic) => {
+    //alert(JSON.stringify(msg) + " @ " +  JSON.stringify(this.state.messages)+" @ " + JSON.stringify(topic));
+    this.setState(prevState => ({
+      messages: [...prevState.messages, msg]
+    }));
+  }
+
+  sendMessage = (msg, selfMsg) => {
+    try {
+      var send_message = {
+        "user" : selfMsg.author,
+        "message" : selfMsg.message,
+        "channelNo" : this.channelNo
+      }
+      this.clientRef.sendMessage("/app/message", JSON.stringify(send_message));
+      return true;
+    } catch(e) {
+      return false;
+    }
+  }
+  
+  componentWillMount() {
+    console.log("call history");
+    Fetch("http://ec2-18-213-155-163.compute-1.amazonaws.com:9090/messenger/history/"+this.channelNo , {
+      method: "GET"
+    }).then((response) => {
+      response.body.forEach(element => {
+        element.author=element.userNo;  // 추후 서버에서 userNo으로 userName을 찾아서 같이 내려올예정 (몽고db에 같이넣을까 체크해바야함)
+      });
+      this.setState({ messages: response.body.reverse() });
+      console.log(response.body)
+      
+    });
+  }
+
+  render() {
+    const wsSourceUrl = "http://ec2-18-213-155-163.compute-1.amazonaws.com:9090/chatting";
+    return (
+      <div>
+        <TalkBox topic={"/topic/public/"+this.channelNo} currentUserId={ this.randomUserId }
+          currentUser={ this.randomUserName } messages={ this.state.messages }
+          onSendMessage={ this.sendMessage } connected={ this.state.clientConnected }/>
+        
+        <SockJsClient url={ wsSourceUrl } topics={["/topic/public/"+this.channelNo]}
+          onMessage={ this.onMessageReceive } ref={ (client) => { this.clientRef = client }}
+          onConnect={ () => {this.setState({ clientConnected: true }) } }
+          onDisconnect={ () => { this.setState({ clientConnected: false }) } }
+          debug={ false } style={[{width:'100%', height:'100%'}]}/>
+      </div>
+    );
+  }
+}
+
+export default App;
